7c1429c9b8ad1cb03feb00194b7ec3175e7d6880
nadjibenmiri <nbenmiri@gmail.com>
2026-03-01 13:29:44 +0100

    Update playback-state.entity.ts

--- Salon/SalonMerged/back/src/entities/playback-state.entity.ts
+++ Salon/SalonMerged/back/src/entities/playback-state.entity.ts
@@ -39,7 +39,7 @@ export class PlaybackState {
   })
   status: PlayStatus;
 
-  @Column({ type: 'int', default: 0 })
+  @Column({ type: 'float', default: 0 })
   positionSec: number;
 
   @Column({ type: 'float', default: 1.0 })
